7094c714e18460b51e3c84cd0493e5816f54bda9
kiyaye1 <kiyaye1@gmail.com>
2025-02-16 01:39:58 -0500

    Initial back-end logic implemented

--- back-end/server.js
+++ back-end/server.js
@@ -3,21 +3,23 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin");
 const appointmentRoutes = require("./routes/appointment");
-const verifyToken = require("./middleware/authMiddleware"); // JWT Middleware
+const verifyToken = require("./middleware/authMiddleware"); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Apply JWT Authentication Globally (Except for Public Routes)
-app.use("/auth", authRoutes); // Public Routes
-app.use(verifyToken); // Apply JWT Authentication Middleware Globally
+// Public Routes
+app.use("/auth", authRoutes); 
+
+// Apply JWT Authentication Globally
+app.use(verifyToken); 
 
 // Protected Routes
 app.use("/admin", adminRoutes);
 app.use("/appointment", appointmentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
